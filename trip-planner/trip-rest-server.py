2c2b5d86f2bc61f4a654114d1f540df58ad6e4db
Katherine Deegan <katherinedeegan@Katherines-MacBook-Pro.local>
2024-11-21 18:40:34 -0700

    adding DB access to trip-rest-server.py. getTrip done.

--- trip-planner/trip-rest-server.py
+++ trip-planner/trip-rest-server.py
@@ -3,10 +3,40 @@
 from flask import Flask, request, Response
 import jsonpickle
 import logging
+from google.cloud.sql.connector import Connector
+import pg8000
+import sqlalchemy
+from sqlalchemy import text
+import json
 
 app = Flask(__name__)
 
 app.logger.setLevel(logging.DEBUG)
+
+# initialize Connector object
+connector = Connector()
+
+# function to return the database connection
+def getconn():
+    conn = connector.connect(
+        "trip-planner-442220:us-central1:trip-planner-db",
+        "pg8000",
+        user="postgres",
+        password="TripPl4nn3r!",
+        db="postgres"
+    )
+    return conn
+
+# create connection pool
+pool = sqlalchemy.create_engine(
+    "postgresql+pg8000://",
+    creator=getconn,
+)
+
+# Test the connection
+with pool.connect() as conn:
+    result = conn.execute(text("SELECT 1"))
+    print(result.fetchone())
 
 @app.route('/apiv1/trip', methods=['POST'])
 def createTrip():
@@ -36,13 +66,30 @@ def updateTrip(tripId, dateString):
 
 @app.route('/apiv1/trip/<tripName>', methods=['GET'])
 def getTrip(tripName):
-    # retrieves Trip record from trip_overview DB
+    # retrieves Trip record from trip_overview DB by tripName (trip name unique in database)
     # returns JSON response containing trip_id, trip_name, start_date, end_date
     app.logger.info(f"Retrieving trip overview information for trip '{tripName}'...")
 
-    # UPDATE
-    response = {'TRIp id' : 100}
-    response_pickled = jsonpickle.encode(response)
+    query = sqlalchemy.text('SELECT * FROM "trip_overview" WHERE trip_name = :tripname')
+
+    with pool.connect() as db_conn:
+        # retrieve trip overview 
+        trip_overview_data = db_conn.execute(query, {"tripname":tripName}).fetchall()
+
+        # if no tripName is found
+        if not trip_overview_data:
+            error_resp = {'error': 'Trip not found', 'message': 'No trip data found for the given trip name.'}
+            return Response(response=jsonpickle.encode(error_resp), status=404, mimetype="application/json")
+
+        trip = trip_overview_data[0
+                                  ]
+        # formulate json response
+        json_resp = {'trip_id' : trip[0], 'trip_name' : str(trip[1]), 'start_date' : str(trip[2]), 'end_date' : str(trip[3]) }
+
+        db_conn.commit()
+
+
+    response_pickled = jsonpickle.encode(json_resp)
     return Response(response=response_pickled, status=200, mimetype="application/json")
 
 @app.route('/apiv1/tripDays/<int:tripId>', methods=['GET'])
